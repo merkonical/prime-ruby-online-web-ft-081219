f8ca422e10bb4fbbb58cb1a1b0013a1ca3949c16
Maverick Adolfo <merkonical@gmail.com>
2019-08-21 03:40:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false 
+  else 
+    (2..num-1).to_a.all? do |factor|
+      num % factor != 0 
+    end
+  end 
+end 
+
+
+
+#Prime number is a number that is not factorable, ex. of prime is 1,2,3,5,7,9
+#non-prime = 4,6,8,10 
